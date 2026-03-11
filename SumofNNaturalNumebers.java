c65adff0261d44d4808e197e1e4ee4a0e83cf7dd
Chandru C <chandru15062004@gmail.com>
2026-03-11 10:36:50 +0530

    Add files via upload
    Signed-off-by: Chandru C <chandru15062004@gmail.com>

---
+++ SumofNNaturalNumebers.java
@@ -0,0 +1,13 @@
+
+public class SumofNNaturalNumebers {
+    public static void main(String[] args) {
+        int num = 15;
+        int sum = 0;
+        for (int i = 0; i <=num; i++) {
+            sum += i;
+
+        }
+        System.out.println(sum);
+    }
+
+}
